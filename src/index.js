64a06529b5204f05e6b0f1832b4f9a46ff0ace7d
sccvn <82246680+sccvn@users.noreply.github.com>
2023-02-02 12:19:37 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+let Parser = require("rss-parser");
+let parser = new Parser();
+
+(async () => {
+  let feed = await parser.parseURL("https://www.reddit.com/.rss");
+  console.log(feed.title);
+
+  feed.items.forEach(item => {
+    console.log(item.title + ":" + item.link);
+  });
+})();
+
+const url = "https://medium.com/feed/towards-data-science/tagged/textmining";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { news: [] };
+  }
+
+  componentDidMount() {
+    this.getNews();
+  }
+
+  async getNews() {
+    const text = await fetch(url).then(r => r.text());
+    const xmlDoc = new DOMParser().parseFromString(text, "text/xml");
+    const items = Array.from(xmlDoc.querySelectorAll("item")).map(item => ({
+      title: item.querySelector("title").textContent,
+      description: item.querySelector("description").childNodes[0].data
+    }));
+    this.setState({ news: items });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">News</h1>
+        </header>
+        <div className="App-feeds" />
+        <div className="panel-list">
+          {this.state.news.length === 0 && <p>Loading...</p>}
+          {this.state.news.map(item => (
+            <div key={item.title}>
+              <h3>{item.title}</h3>
+              <div dangerouslySetInnerHTML={{ __html: item.description }} />
+              <hr />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
