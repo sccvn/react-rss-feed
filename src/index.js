7db19e39939919d73de6fd6bf3bd42c76d7e4331
Tuan Nguyen Anh <kevinnguyen.asian.vie@gmail.com>
2023-02-02 07:01:53 +0000

    Update 2 files

--- src/index.js
+++ src/index.js
@@ -10,12 +10,14 @@ let parser = new Parser();
   let feed = await parser.parseURL("https://www.reddit.com/.rss");
   console.log(feed.title);
 
-  feed.items.forEach(item => {
+  feed.items.forEach((item) => {
     console.log(item.title + ":" + item.link);
   });
 })();
 
-const url = "https://medium.com/feed/towards-data-science/tagged/textmining";
+const url = "https://rss.art19.com/apology-line";
+let headers = new Headers();
+headers.append("Access-Control-Allow-Origin", "http://localhost:3000");
 
 class App extends React.Component {
   constructor(props) {
@@ -28,11 +30,18 @@ class App extends React.Component {
   }
 
   async getNews() {
-    const text = await fetch(url).then(r => r.text());
+    const text = await fetch(url, {
+      //mode: 'no-cors',
+      // credentials: 'include',
+      method: "GET",
+      headers: headers,
+    })
+      .then((r) => r.text())
+      .catch((err) => console.log(err));
     const xmlDoc = new DOMParser().parseFromString(text, "text/xml");
-    const items = Array.from(xmlDoc.querySelectorAll("item")).map(item => ({
+    const items = Array.from(xmlDoc.querySelectorAll("item")).map((item) => ({
       title: item.querySelector("title").textContent,
-      description: item.querySelector("description").childNodes[0].data
+      description: item.querySelector("description").childNodes[0].data,
     }));
     this.setState({ news: items });
   }
@@ -46,7 +55,7 @@ class App extends React.Component {
         <div className="App-feeds" />
         <div className="panel-list">
           {this.state.news.length === 0 && <p>Loading...</p>}
-          {this.state.news.map(item => (
+          {this.state.news.map((item) => (
             <div key={item.title}>
               <h3>{item.title}</h3>
               <div dangerouslySetInnerHTML={{ __html: item.description }} />
